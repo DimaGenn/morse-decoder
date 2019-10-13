4f90a450576cf444de37ddb684766bfaa425dd1d
DimaGenn <dgsharapov@gmail.com>
2019-10-13 20:59:44 +0300

    add function decode

--- src/index.js
+++ src/index.js
@@ -37,8 +37,25 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
+
+
+
 function decode(expr) {
-    // write your solution here
+    let array = expr.split('**********');
+    let result = "";
+    for (i = 0; i < array.length; i++) {
+      for (j = 0; j < array[i].length; j += 10) {
+        let numbs = array[i][j] + array[i][j + 1] + array[i][j + 2] + array[i][j + 3] + array[i][j + 4] + array[i][j + 5] + array[i][j + 6] + array[i][j + 7] + array[i][j + 8] + array[i][j + 9];
+        let symbols = numbs.replace(/11/g, "-").replace(/10/g, ".").replace(/0/g, "");
+        symbols = MORSE_TABLE[symbols];
+        result = result + symbols;
+      }
+      result = result + " ";
+    }
+    result = result.slice(0, -1);
+  return(result);
 }
 
 module.exports = {
